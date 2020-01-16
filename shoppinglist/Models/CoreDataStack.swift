39e4de12cf32a774b13bdd920592295416794837
jon <jdcorn@live.com>
2020-01-16 16:51:20 -0700

    first commit

---
+++ shoppinglist/Models/CoreDataStack.swift
@@ -0,0 +1,26 @@
+//
+//  CoreDataStack.swift
+//  shoppinglist
+//
+//  Created by Jon Corn on 1/16/20.
+//  Copyright © 2020 jdcorn. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+    static let container: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "shoppinglist")
+        container.loadPersistentStores() { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    static var context: NSManagedObjectContext {return container.viewContext}
+}
---
+++ shoppinglist/Models/Item+Convenience.swift
@@ -0,0 +1,10 @@
+//
+//  shoppinglist+convenience.swift
+//  shoppinglist
+//
+//  Created by Jon Corn on 1/16/20.
+//  Copyright © 2020 jdcorn. All rights reserved.
+//
+
+import Foundation
+
