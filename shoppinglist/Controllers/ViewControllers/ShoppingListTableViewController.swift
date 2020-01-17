64d8a1b81dd3a8ff33200eb86be65f518fc0c323
jon <jdcorn@live.com>
2020-01-17 09:27:05 -0700

    conformed tableview to cell protocol

--- shoppinglist/Controllers/ViewControllers/ShoppingListTableViewController.swift
+++ shoppinglist/Controllers/ViewControllers/ShoppingListTableViewController.swift
@@ -7,14 +7,21 @@
 //
 
 import UIKit
+import CoreData
 
 class ShoppingListTableViewController: UITableViewController {
 
     // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        ItemController.shared.fetchedResultsController.delegate = self
     }
-
+    
+    // MARK: - Actions
+    @IBAction func addButtonTapped(_ sender: Any) {
+        
+    }
+    
     // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return ItemController.shared.fetchedResultsController.fetchedObjects?.count ?? 0
@@ -22,9 +29,10 @@ class ShoppingListTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "itemCell", for: indexPath) as? ItemTableViewCell else { return UITableViewCell() }
-
-        let item = ItemController.shared.fetchedResultsController.fetchedObjects?[indexPath.row]
-
+        
+        let item = ItemController.shared.fetchedResultsController.object(at: indexPath)
+        cell.updateViews(item: item)
+        cell.delegate = self
         return cell
     }
 
@@ -37,4 +45,60 @@ class ShoppingListTableViewController: UITableViewController {
     }
 }
 
+// MARK: - NSFetchedResultsControllerDelegate
+extension ShoppingListTableViewController: NSFetchedResultsControllerDelegate {
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+        
+        let indexSet = IndexSet(integer: sectionIndex)
+        
+        switch type {
+        case .insert:
+            tableView.insertSections(indexSet, with: .fade)
+        case .delete:
+            tableView.deleteSections(indexSet, with: .fade)
+            
+        default: return
+        }
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        
+        switch type {
+        case .insert:
+            guard let newIndexPath = newIndexPath
+                else { return }
+            tableView.insertRows(at: [newIndexPath], with: .fade)
+        case .delete:
+            guard let indexPath = indexPath
+                else { return }
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        case .update:
+            guard let indexPath = indexPath
+                else { return }
+            tableView.reloadRows(at: [indexPath], with: .none)
+        case .move:
+            guard let indexPath = indexPath, let newIndexPath = newIndexPath
+                else { return }
+            tableView.moveRow(at: indexPath, to: newIndexPath)
+        @unknown default:
+            fatalError("NSFetchedResultsChangeType has new unhandled cases")
+        }
+    }
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+}
+
+extension ShoppingListTableViewController: ItemTableViewCellDelegate {
+    func isPurchasedToggled(_ sender: ItemTableViewCell) {
+        guard let indexPath = tableView.indexPath(for: sender) else { return }
+        let item = ItemController.shared.fetchedResultsController.object(at: indexPath)
+        ItemController.shared.toggle(item: item)
+        sender.updateViews(item: item)
+    }
+}
 
