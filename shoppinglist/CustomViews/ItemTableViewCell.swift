fc69dbaddefaef5f1eec877d3047f5dcc04c191d
jon <jdcorn@live.com>
2020-01-17 09:18:16 -0700

    table view cell cleanup

--- shoppinglist/CustomViews/ItemTableViewCell.swift
+++ shoppinglist/CustomViews/ItemTableViewCell.swift
@@ -8,8 +8,30 @@
 
 import UIKit
 
+protocol ItemTableViewCellDelegate: class {
+    func isPurchasedToggled(_ sender: ItemTableViewCell)
+}
+
 class ItemTableViewCell: UITableViewCell {
+    // MARK: - Properties
+    weak var delegate: ItemTableViewCellDelegate?
     
+    // MARK: - Outlets
+    @IBOutlet weak var itemNameLabel: UILabel!
+    @IBOutlet weak var isPurchasedButton: UIButton!
     
-    
+    // MARK: - Actions
+    @IBAction func isPurchasedButtonTapped(_ sender: Any) {
+        delegate?.isPurchasedToggled(self)
+    }
+
+    // MARK: - Functions
+    func updateViews(item: Item) {
+        itemNameLabel.text = item.name
+        if item.isPurchased {
+            isPurchasedButton.setImage(#imageLiteral(resourceName: "checked"), for: .normal)
+        } else {
+            isPurchasedButton.setImage(#imageLiteral(resourceName: "uncheck"), for: .normal)
+        }
+    }
 }
